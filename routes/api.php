a8d23b77112aa298134edb3e940556cdfea3a824
haryarrahman <haryarrahmman7@gmail.com>
2024-06-02 21:48:56 +0700

    feat : API products

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ 
+
+Route::get('products', [ProductController::class, 'all']);
+Route::get('products/{id}', [ProductController::class, 'detail']);
