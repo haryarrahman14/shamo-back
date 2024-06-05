e2292830231fbafcc9a977cf1a3e4f8c1a71cfc3
hary <haryarrahman14@gmail.com>
2024-06-05 14:31:59 +0700

    feat : API transactions

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        $transaction = Transaction::with(['items.product'])->where('user_id', $request->user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data transaksi berhasil diambil'
+        );
+    }
+
+    public function find($id)
+    {
+        $transaction = Transaction::with(['items.product'])->find($id);
+
+        if ($transaction) {
+            return ResponseFormatter::success(
+                $transaction,
+                'Data transaksi berhasil diambil'
+            );
+        } else {
+            return ResponseFormatter::error(
+                null,
+                'Data transaksi tidak ada',
+                404
+            );
+        }
+    }
+}
